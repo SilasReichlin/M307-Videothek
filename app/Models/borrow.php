546388051da1c1c8895b9500a1f63c88ecb004b3
Silas Reichlin <Silas.Reichlin@sage.com>
2022-05-10 09:49:06 +0200

    fixed errors

--- app/Models/borrow.php
+++ app/Models/borrow.php
@@ -14,13 +14,13 @@ class Borrow
     //constructors
     public function __construct()
     {
-        $this->db = db($this->table);
+        $this->db = db('videothek');
     }
 
     //functions
     public function getAllBorrows() : array
     {
-        $statement = $this->db->prepare('SELECT * FROM ausleihe');
+        $statement = $this->db->prepare("SELECT * FROM $this->table");
         $statement->execute();
 
         return $statement->fetchAll();
@@ -28,7 +28,7 @@ class Borrow
 
     public function createBorrow() : void
     {
-        $statement = $this->db->prepare('INSERT INTO ausleihe (name, email, telefon) VALUES (:name :email :telefon;)');
+        $statement = $this->db->prepare("INSERT INTO $this->table (name, email, telefon) VALUES (:name :email :telefon;)");
         $statement->bindParam(':name', $this->name);
         $statement->bindParam(':email', $this->email);
         $statement->bindParam(':telefon', $this->telefon);
